413bb489c2def9d602daa6a178b25ed2d4c8ed9e
Muhamed Fejzic <91850725+mfejzic@users.noreply.github.com>
2024-10-03 16:05:58 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -16,8 +16,8 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "064b2bea-3fc1-4bf7-b067-3d942c23e4dd"
-  tenant_id = "16983dae-9f48-4a35-b9f5-0519bf3cdf09"
-  client_id = "a4815ff2-fc06-4608-b1c6-9b902ac9ffb3"
-  client_secret = "MWh8Q~ga.sD4kz6HEvOalPL_cwYTZJXxX47eZbkX"
+  subscription_id = ""
+  tenant_id = ""
+  client_id = ""
+  client_secret = ""
 }
