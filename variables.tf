b0d31c57d93504e87f2dd81208d83271b7af1682
Muhamed Fejzic <91850725+mfejzic@users.noreply.github.com>
2024-10-19 17:03:28 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,19 +1,51 @@
-variable "resource_group_name" {
-  type=string
-  description="This defines the resource group name"
+variable "East_US" {
+  type    = string
+  default = "eastus"
+}
+variable "East_US_2" {
+  type    = string
+  default = "eastus2"
+}
+variable "West_US" {
+  type    = string
+  default = "westus"
+}
+variable "Central_US" {
+  type    = string
+  default = "centralus"
+}
+variable "northeurope" {
+  type    = string
+  default = "northeurope"
 }
 
-variable "location" {
-  type=string
-  description="This defines the location"
+
+
+variable "num_of_sub_it1" {
+  type        = number
+  description = "defines number of subnets"
+  default     = 3 // makes three subnets
 }
 
-variable "virtual_network_name" {
-  type=string
-  description="This defines the name of the virtual network"
+variable "num_of_sub_it2" {
+  type        = number
+  description = "defines number of subnets"
+  default     = 4 // makes three subnets
+  validation {
+    condition     = var.num_of_sub_it2 < 5
+    error_message = "number of subnets must be less than 5"
+  }
 }
 
-variable "virtual_network_address_space" {
-  type=string
-  description="This defines the address space of the virtual network"
+variable "num_of_vms" {
+  type        = number
+  description = "number of virtual machines"
+  default     = 3
+}
+
+# new network infra variables
+variable "number_of_machines" {
+  type        = number
+  description = "This defines the number of virtual machines in the virtual network"
+  default     = 2
 }
