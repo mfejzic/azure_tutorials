6755f0e396580816635161701bcf199fef7e6893
Muhamed Fejzic <91850725+mfejzic@users.noreply.github.com>
2024-10-28 13:45:36 -0500

    Update variables.tf

--- monolith_modules/general/variables.tf
+++ monolith_modules/general/variables.tf
@@ -1,1 +1,10 @@
 
+variable "resource_group_name" {
+  type = string
+  description = "defines resource group name"
+}
+
+variable "resource_group_location" {
+  type = string
+  description = "defines resource group location"
+}
