f50b4d5abf791fb3163a623435afce47b99b77b2
Muhamed Fejzic <91850725+mfejzic@users.noreply.github.com>
2024-11-11 16:17:52 -0600

    Update monolith.tf

--- monolith.tf
+++ monolith.tf
@@ -1,4 +1,3 @@
-## ! BIG STUFF HAPPENING ! ##
 # manually build a web server - create an image of that server
    # create 2019 windows datacenter vm
    # go to tools -> IIS manager -> webvm -> management service -> enable remote connection -> port should be 8172-> click on apply/start
@@ -7,65 +6,98 @@
 
 locals {
   resource_group_name = "main_resource_group"
-  location = "EASTUS"
+  location            = "WESTUS"
   virtual_network = {
     address_space = "10.0.0.0/16"
   }
 }
 
 module "general_module" {
-    source = "./monolith_modules/general"
-    resource_group_name = local.resource_group_name
-    resource_group_location = local.location
+  source                  = "./monolith_modules/general"
+  resource_group_name     = local.resource_group_name
+  resource_group_location = local.location
 }
 
 module "vnet" {
-  source = "./monolith_modules/vnet"
+  source              = "./monolith_modules/vnet"
   resource_group_name = local.resource_group_name
-  vnet_name = "main_vnet"
-  vnet_location = local.location
-  vnet_address_space = "10.0.0.0/16"
-  subnet_names = ["web-subnet", "db-subnet"]
-  bastion_required = true                                 # false by default
+  vnet_name           = "main_vnet"
+  vnet_location       = local.location
+  vnet_address_space  = "10.0.0.0/16"
+  subnet_names        = ["web-subnet", "db-subnet"]
+  bastion_required    = true # false by default
 
   #use type map(string)
   nsg_names = {
     "web-nsg" = "web-subnet"
-    "db-nsg" = "db-subnet"
-   }
+    "db-nsg"  = "db-subnet"
+  }
 
-   nsg_rules = [{
-    id = 1,
-    priority = "200"
+  nsg_rules = [{
+    id                          = 1,
+    priority                    = "200"
     network_security_group_name = "web-nsg"
-    destination_port_range = "3389"
-    access = "Allow"
-   }, {
-    id = 2,
-    priority = "300"
+    destination_port_range      = "3389"
+    access                      = "Allow"
+    }, {
+    id                          = 2,
+    priority                    = "300"
     network_security_group_name = "web-nsg"
-    destination_port_range = "80"
-    access = "Allow"
-   }, {
-    id = 3,
-    priority = "400"
+    destination_port_range      = "80"
+    access                      = "Allow"
+    }, {
+    id                          = 3,
+    priority                    = "400"
     network_security_group_name = "web-nsg"
-    destination_port_range = "8173"
-    access = "Allow"
-   }, {
-    id = 4,
-    priority = "200"
+    destination_port_range      = "8173"
+    access                      = "Allow"
+    }, {
+    id                          = 4,
+    priority                    = "200"
     network_security_group_name = "db-nsg"
-    destination_port_range = "3389"
-    access = "Allow"
-   }]
+    destination_port_range      = "3389"
+    access                      = "Allow"
+  }]
 }
 # manually defines subnet space
-  /* subnet_names = {
+/* subnet_names = {
     "web-subnet" = "10.0.0.0/24"
     "db-subnet" = "10.0.1.0/24"
   } */
 
+
+module "compute" {
+  source              = "./monolith_modules/compute"
+  vnet_location       = local.location
+  resource_group_name = local.resource_group_name
+  db_subnet_id = module.vnet.subnets["db-subnet"].id
+  nic_name = "db-nic"
+  publicip_name = "db_name"
+  publicip_required = false
+  vmdb_name = "db-vm"
+  admin_username = "adminuser"
+  admin_password = "P@$$w0rd1234!"
+
+  source_image_reference = {
+    publisher = "MicrosoftSQLServer"
+    offer     = "sql2019-ws2019"
+    sku       = "sqldev"
+    version   = "latest"
+  }
+
+  depends_on = [ module.vnet ]
+
+}
+
+resource "azurerm_mssql_virtual_machine" "mysql_vm" {
+  virtual_machine_id               = module.compute.db_vm.id
+  sql_license_type   = "PAYG"
+  sql_connectivity_update_password = "Azure@1234"
+  sql_connectivity_update_username = "sqladmin"
+  sql_connectivity_port            = 1433
+  sql_connectivity_type            = "PRIVATE"
+
+}
 
 
 resource "azurerm_network_interface" "image_interface" {
@@ -97,8 +129,8 @@ resource "azurerm_public_ip" "image_publicip" {
 data "azurerm_shared_image" "image" {
   name                = "defineimage1"
   gallery_name        = "imagegallery"
-  resource_group_name = local.resource_group_name
-  depends_on = [ module.general_module.main_RG ]
+  resource_group_name = "new-grp"
+  depends_on          = [module.general_module.main_RG]
 }
 
 # create a virtual machine from the image
@@ -108,9 +140,11 @@ resource "azurerm_virtual_machine" "webvm" {
   resource_group_name   = local.resource_group_name
   network_interface_ids = [azurerm_network_interface.image_interface.id]
   vm_size               = "Standard_DS1_v2"
+  
 
   storage_image_reference {
     id = data.azurerm_shared_image.image.id
+    
   }
   storage_os_disk {
     name              = "myosdisk1"
