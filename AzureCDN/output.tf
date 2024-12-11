e1c1ddd5d94bca6ab255420dfcf3258c30f56ccb
Muhamed Fejzic <91850725+mfejzic@users.noreply.github.com>
2024-12-10 22:21:57 -0600

    Update output.tf

--- AzureCDN/output.tf
+++ AzureCDN/output.tf
@@ -1,1 +1,10 @@
 
+output "dns_zone_name" {
+  value = data.azurerm_dns_zone.dns_zone.name
+  description = "The name of the DNS zone"
+}
+
+output "resource_group_name" {
+  value = data.azurerm_resource_group.main_RG.name
+  description = "The name of the resource group"
+}
