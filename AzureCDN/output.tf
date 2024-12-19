5cc6d9689f72d2ae49fbca533836dbc6bb12f348
Muhamed Fejzic <91850725+mfejzic@users.noreply.github.com>
2024-12-18 19:51:27 -0600

    Update output.tf

--- AzureCDN/output.tf
+++ AzureCDN/output.tf
@@ -1,10 +1,17 @@
+# output "dns_zone_name" {
+#   value = data.azurerm_dns_zone.dns_zone.name
+#   description = "The name of the DNS zone"
+# }
 
-output "dns_zone_name" {
-  value = data.azurerm_dns_zone.dns_zone.name
-  description = "The name of the DNS zone"
+# output "resource_group_name" {
+#   value = data.azurerm_resource_group.main_RG.name
+#   description = "The name of the resource group"
+# }
+
+output "secondary_web_endpoint" {
+  value = azurerm_storage_account.SA_east.secondary_web_endpoint
 }
 
-output "resource_group_name" {
-  value = data.azurerm_resource_group.main_RG.name
-  description = "The name of the resource group"
+output "primary_web_endpoint" {
+  value = azurerm_storage_account.SA_west.primary_web_endpoint
 }
