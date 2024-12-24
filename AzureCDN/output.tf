83cc9491642be6bef2b345d75c6df1c39742ec32
Muhamed Fejzic <91850725+mfejzic@users.noreply.github.com>
2024-12-23 18:52:24 -0600

    Update output.tf

--- AzureCDN/output.tf
+++ AzureCDN/output.tf
@@ -12,7 +12,26 @@
 output "secondary_web_endpoint" {
   value = azurerm_storage_account.SA_east.secondary_web_endpoint
 }
+output "primary_endpoint_fqdn" {
+  value = azurerm_cdn_endpoint.primary_endpoint.fqdn  
+}
 
+output "cdn_primary_endpoint_origin" {
+  value = azurerm_cdn_endpoint.primary_endpoint.origin
+}
+
+output "primary_web_host" {
+  value = azurerm_storage_account.SA_west.primary_web_host
+}
+output "primary_blob_host" {
+  value = azurerm_storage_account.SA_west.primary_blob_host
+}
 output "primary_web_endpoint" {
   value = azurerm_storage_account.SA_west.primary_web_endpoint
+}
+output "primary_blob_endpoint" {
+  value = azurerm_storage_account.SA_west.primary_blob_endpoint
+}
+output "primary_file_host" {
+  value = azurerm_storage_account.SA_west.primary_file_host
 }
