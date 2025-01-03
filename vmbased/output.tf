d2207a72fe58f02c0248fc674b4a82e76b19b906
Muhamed Fejzic <91850725+mfejzic@users.noreply.github.com>
2025-01-03 16:56:12 -0600

    Update output.tf

--- vmbased/output.tf
+++ vmbased/output.tf
@@ -1,1 +1,20 @@
 
+output "subnets_zone1" {
+  value       = local.subnets_zone1
+  description = "List of subnets in Zone 1"
+}
+
+output "subnets_zone2" {
+  value       = local.subnets_zone2
+  description = "List of subnets in Zone 2"
+}
+
+output "public_subnets" {
+  value = local.public_subnets
+  description = "List of public subnet names in zone 1 and zone 2"
+}
+
+output "zone_names" {
+  value = local.zones
+  description = "List of zone names"
+}
